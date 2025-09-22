9f9ae829bb67b1416833b69c89e8665873a2a221
JangBJ <qudwnd1001@naver.com>
2025-09-22 20:43:58 +0900

    요구 사항 7 구현

--- web-application-server-gradle/src/main/java/webserver/RequestHandler.java
+++ web-application-server-gradle/src/main/java/webserver/RequestHandler.java
@@ -21,7 +21,6 @@ public class RequestHandler extends Thread {
     private Socket connection;
 
     private static final String BASE_REDIRECT_URL = "/index.html";
-    private static final String LOGIN_URL = "/user/login.html";
     private static final String LOGIN_FAILED_URL = "/user/login_failed.html";
 
     public RequestHandler(Socket connectionSocket) {
@@ -89,14 +88,12 @@ public class RequestHandler extends Thread {
                 responseBody(dos, body);
             }
             else if(requestUrl.equals("/user/login")){
-                // 여기서도 post 요청이기에 해당하는 값들이 넘어오는걸 파싱해서 사용해아함
                 String httpBody = IOUtils.readData(reader, contentLength);
                 Map<String, String> paramsMap = HttpRequestUtils.parseQueryString(httpBody);
 
                 String userId = paramsMap.get("userId");
                 String password = paramsMap.get("password");
 
-                // DB에 있는 User 객체와 같은지 찾아야함
                 User user = DataBase.findUserById(userId);
                 if(user == null){
                     requestUrl = LOGIN_FAILED_URL;
@@ -121,9 +118,6 @@ public class RequestHandler extends Thread {
                 }
             }
             else if(requestUrl.equals("/user/list")){
-                // 1. 쿠키 값을 이용해 해당 유저가 로그인 상태인지 판단.
-                // true 라면 list.html로 false라면 로그인 페이지로 이동시킨다.
-                // 리스트 항목을 보여주는 것은 StringBuilder 클래스를 활용한다.
                 if(!logined){
                     requestUrl = BASE_REDIRECT_URL;
                     DataOutputStream dos = new DataOutputStream(out);
@@ -143,10 +137,16 @@ public class RequestHandler extends Thread {
                 }
                 sb.append("</table>");
                 DataOutputStream dos = new DataOutputStream(out);
-                byte[] body = sb.toString().getBytes(StandardCharsets.UTF_8);
+                byte[] body = sb.toString().getBytes();
                 response200Header(dos, body.length);
                 responseBody(dos, body);
 
+            }
+            else if(url.endsWith(".css")){
+                DataOutputStream dos = new DataOutputStream(out);
+                byte[] body = Files.readAllBytes(new File("./webapp"+requestUrl).toPath());
+                response200CssHandler(dos, body.length);
+                responseBody(dos, body);
             }
             else {
                 // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
@@ -200,6 +200,17 @@ public class RequestHandler extends Thread {
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("Set-Cookie: logined=true\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response200CssHandler(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/css\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
