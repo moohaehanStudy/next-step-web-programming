01c114bba341b3db8b3a20e6893cda1a5ab0473e
soyun <ehouse190@gmail.com>
2025-10-11 22:14:19 +0900

    Functional Interface 어노테이션 추가

--- jwp-basic-gradle/src/main/java/dao/PreparedStatementSetter.java
+++ jwp-basic-gradle/src/main/java/dao/PreparedStatementSetter.java
@@ -1,9 +1,9 @@
 package dao;
 
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface PreparedStatementSetter {
     void setValues(PreparedStatement ps) throws SQLException;
 }
