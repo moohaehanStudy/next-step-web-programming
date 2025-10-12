4a62295f1c4d139d4577771d9d31ec1b29a34375
soyun <ehouse190@gmail.com>
2025-10-12 14:18:54 +0900

    람다식으로 변환

--- jwp-basic-gradle/src/main/java/dao/UserDao.java
+++ jwp-basic-gradle/src/main/java/dao/UserDao.java
@@ -20,13 +20,11 @@ public class UserDao {
     public void insert(User user) throws SQLException {
         JdbcTemplate jdbcTemplate = new JdbcTemplate();
 
-        PreparedStatementSetter pss = new PreparedStatementSetter() {
-            public void setValues(PreparedStatement ps) throws SQLException {
-                ps.setString(1, user.getUserId());
-                ps.setString(2, user.getPassword());
-                ps.setString(3, user.getName());
-                ps.setString(4, user.getEmail());
-            }
+        PreparedStatementSetter pss = ps -> {
+            ps.setString(1, user.getUserId());
+            ps.setString(2, user.getPassword());
+            ps.setString(3, user.getName());
+            ps.setString(4, user.getEmail());
         };
 
         jdbcTemplate.update(INSERTQUERY, pss);
@@ -35,13 +33,11 @@ public class UserDao {
     public void update(User user) throws SQLException {
         JdbcTemplate jdbcTemplate = new JdbcTemplate();
 
-        PreparedStatementSetter pss = new PreparedStatementSetter() {
-            public void setValues(PreparedStatement ps) throws SQLException {
-                ps.setString(1, user.getPassword());
-                ps.setString(2, user.getName());
-                ps.setString(3, user.getEmail());
-                ps.setString(4, user.getUserId());
-            }
+        PreparedStatementSetter pss = ps -> {
+            ps.setString(1, user.getPassword());
+            ps.setString(2, user.getName());
+            ps.setString(3, user.getEmail());
+            ps.setString(4, user.getUserId());
         };
 
         jdbcTemplate.update(UPDATEQUERY, pss);
@@ -50,10 +46,8 @@ public class UserDao {
     public User findByUserId(String userId) throws SQLException {
         JdbcTemplate jdbcTemplate = new JdbcTemplate();
 
-        PreparedStatementSetter pss = new PreparedStatementSetter() {
-            public void setValues(PreparedStatement ps) throws SQLException {
-                ps.setString(1, userId);
-            }
+        PreparedStatementSetter pss = ps -> {
+            ps.setString(1, userId);
         };
 
         RowMapper<User> rm = createRowMapper();
@@ -62,32 +56,19 @@ public class UserDao {
     }
 
     private RowMapper<User> createRowMapper() {
-        RowMapper<User> rm = new RowMapper<User>(){
-            public User mapRow(ResultSet rs) throws SQLException {
-                return new User(
-                        rs.getString(USERID),
-                        rs.getString(PASSWORD),
-                        rs.getString(NAME),
-                        rs.getString(EMAIL)
-                );
-            }
-        };
-
-        /* 람다식으로 변환 시
         RowMapper<User> rm = rs -> new User(
                 rs.getString(USERID),
                 rs.getString(PASSWORD),
                 rs.getString(NAME),
                 rs.getString(EMAIL)
         );
-        */
+
         return rm;
     }
 
     public List<User> findAll() throws SQLException {
         JdbcTemplate jdbcTemplate = new JdbcTemplate();
 
-        // 람다식으로 변환
         PreparedStatementSetter pss = ps -> {};
 
         RowMapper<User> rm = createRowMapper();
